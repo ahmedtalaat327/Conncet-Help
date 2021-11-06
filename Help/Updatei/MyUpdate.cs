ef426ff965d285149b0b6781daea90097ff07987
Ahmed Talaat <ahmedtm2015@gmail.com>
2021-11-06 15:20:59 +0200

    update featue abst-class

---
+++ Help/Updatei/MyUpdate.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Help.Updatei
+{
+    public abstract class MyUpdate
+    {
+
+        /// <summary>
+        /// Check for update intially
+        /// </summary>
+        /// <returns></returns>
+        public virtual bool MyUpdate_CheckForMe(string nameofapp,int cversion) {
+            return false;
+        }
+
+
+
+
+
+    }
+}
