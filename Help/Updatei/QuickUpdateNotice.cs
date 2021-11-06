56f27bc1e780ce53fcb9ad307ffb6c0f8813a68d
Ahmed Talaat <ahmedtm2015@gmail.com>
2021-11-06 15:29:45 +0200

    update feautre!

---
+++ Help/Updatei/QuickUpdateNotice.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Help.Updatei
+{
+    class QuickUpdateNotice : MyUpdate
+    {
+        public QuickUpdateNotice(string appName)
+        {
+
+        }
+
+        public override bool MyUpdate_CheckForMe(string nameofapp, int cversion)
+        {
+            return false;
+        }
+
+        public override void MyUpdate_GrabNextVersion()
+        {
+            
+        }
+    }
+}
